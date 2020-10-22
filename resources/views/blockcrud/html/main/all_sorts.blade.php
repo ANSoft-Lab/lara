b19244dba726d6564f7e84cfb3b88da7d907e682
Alexanyasha <shuvalova.alexandra@gmail.com>
2020-10-22 13:45:31 +0300

    Links for best price block

--- resources/views/blockcrud/html/main/all_sorts.blade.php
+++
@@ -1,8 +0,0 @@
-<section class="all-sorts">
-    <h2 class="all-sorts__title{{ isset($edit_mode) ? ' blockcrud-editable' : '' }}">
-        Все виды страхования
-    </h2>
-    <a href="{{ config('app.old_url') }}" class="all-sorts__link{{ isset($edit_mode) ? ' blockcrud-editable' : '' }}">
-        Смотреть все
-    </a>
-</section>
--- resources/views/blockcrud/templates/kis/best_price.blade.php
+++ resources/views/blockcrud/templates/kis/best_price.blade.php
@@ -15,37 +15,37 @@
             {!! $kis_best_2 ?? 'Мы сотрудничаем только с аккредитованными компаниями' !!}
         </h2>
         <div class="partners-logos d-flex flex-wrap justify-content-space-around align-content-center mb-40">
-            <a href="">
+            <a href="https://www.ingos.ru/" target="_blank">
                 <picture>
                     <source srcset="/icon/partners/ingos.svg" media="(min-width: 768px)">
                     <img src="/icon/partners/ingos.svg" alt="Ингосстрах" class="p-logo">
                 </picture>
             </a>
-            <a href="">
+            <a href="https://www.ergo.ee/ru" target="_blank">
                 <picture>
                     <source srcset="/icon/partners/ergo.jpg" media="(min-width: 768px)">
                     <img src="/icon/partners/ergo.jpg" alt="Ergo" class="p-logo">
                 </picture>
             </a>
-            <a href="">
+            <a href="https://www.reso.ru/" target="_blank">
                 <picture>
                     <source srcset="/icon/partners/reso-logo.png" media="(min-width: 768px)">
                     <img src="/icon/partners/reso-logo.png" alt="Ресо" class="p-logo">
                 </picture>
             </a>
-            <a href="">
+            <a href="https://www.alfastrah.ru/" target="_blank">
                 <picture>
                     <source srcset="/icon/partners/alfastrahovanie.jpg" media="(min-width: 768px)">
                     <img src="/icon/partners/alfastrahovanie.jpg" alt="Альфа Страхование" class="p-logo">
                 </picture>
             </a>
-            <a href="">
+            <a href="https://sovcomins.ru/" target="_blank">
                 <picture>
                     <source srcset="/icon/partners/liberty.png" media="(min-width: 768px)">
                     <img src="/icon/partners/liberty.png" alt="Liberty" class="p-logo">
                 </picture>
             </a>
-            <a href="">
+            <a href="https://www.vsk.ru/" target="_blank">
                 <picture>
                     <source srcset="/icon/partners/vsk.jpg" media="(min-width: 768px)">
                     <img src="/icon/partners/vsk.jpg" alt="ВСК" class="p-logo">
@@ -78,25 +78,25 @@
             </div>
             <div class="d-flex flex-wrap shadow-blocks w-md-50">
                 <div class="shadow-block b-radius mr-10 mb-10">
-                    <a href="#">Сбербанк</a>
+                    <a href="https://www.sberbank.ru/" target="_blank">Сбербанк</a>
                 </div>
                 <div class="shadow-block b-radius mr-10 mb-10">
-                    <a href="#">АльфаБанк</a>
+                    <a href="https://alfabank.ru/" target="_blank">АльфаБанк</a>
                 </div>
                 <div class="shadow-block b-radius mr-10 mb-10">
-                    <a href="#">ВТБ</a>
+                    <a href="https://www.vtb.ru/" target="_blank">ВТБ</a>
                 </div>
                 <div class="shadow-block b-radius mr-10 mb-10">
-                    <a href="#">Газпромбанк</a>
+                    <a href="https://www.gazprombank.ru/" target="_blank">Газпромбанк</a>
                 </div>
                 <div class="shadow-block b-radius mr-10 mb-10">
-                    <a href="#">МеталлИнвестБанк</a>
+                    <a href="https://metallinvestbank.ru/" target="_blank">МеталлИнвестБанк</a>
                 </div>
                 <div class="shadow-block b-radius mr-10 mb-10">
-                    <a href="#">Открытие</a>
+                    <a href="https://www.open.ru/" target="_blank">Открытие</a>
                 </div>
                 <div class="shadow-block b-radius mr-10 mb-10">
-                    <a href="#">Уралсиб</a>
+                    <a href="https://www.uralsib.ru/" target="_blank">Уралсиб</a>
                 </div>
             </div>
         </div>
