72265af6a0370c87f9d03a78361c68468587e2e7
levinpwnz <vkpasser@yandex.kz>
2020-12-25 21:19:11 +0800

    upd. 17 row from comments.

--- resources/views/blockcrud/templates/kis/plus_minus.blade.php
+++ resources/views/blockcrud/templates/kis/plus_minus.blade.php
@@ -27,24 +27,5 @@
                 @customblock('feedback')
             </div>
         </div>
-        <div class="plus_minus__panel minus">
-            <h2 class="about__title about__title_tab{{ isset($edit_mode) ? ' blockcrud-editable' : '' }}"
-            @if (isset($edit_mode))
-                contenteditable="true" id="kis_plus_3"
-            @endif
-            >
-                {!! $kis_plus_3 ?? 'Самостоятельное оформление полиса ипотечного страхования это:' !!}
-            </h2>
-            <ul class="plus_minus__list">
-                <li>Потеря времени и сил на поиск подходящей страховой компании;</li>
-                <li>Неправильное оформление документов для страховой компании;</li>
-                <li>Переплата страхового взноса по договору страхования;</li>
-                <li>Необходимость приезжать в офис страховой компании для заключения договора;</li>
-                <li>Самостоятельное решение вопросов со страховой компанией при наступлении страхового случая.</li>
-            </ul>
-            <div class="cooperation__btn-wrap">
-                @customblock('feedback', ['modal_title' => 'Бесплатная консультация', 'btn_class' => 'cooperation__outline'])
-            </div>
-        </div>
     </div>
 </section>
