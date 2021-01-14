f291f9c6fb4c57bf1817e1cdd039331b5203fb76
levinpwnz <vkpasser@yandex.kz>
2021-01-14 17:10:58 +0800

    fix. 14 from comments

--- resources/views/blockcrud/templates/main/reasons.blade.php
+++ resources/views/blockcrud/templates/main/reasons.blade.php
@@ -1,43 +1,23 @@
 <section class="reasons">
     <div class="reasons__text">
         <h2 class="additional__title{{ isset($edit_mode) ? ' blockcrud-editable' : '' }}"
-        @if (isset($edit_mode))
+            @if (isset($edit_mode))
             contenteditable="true" id="main_reasons_1"
-        @endif
+            @endif
         >{!! $main_reasons_1 ?? 'Причины работать с нами' !!}</h2>
     </div>
-    <ul class="reasons__list advan">
-        <li class="reasons__list-item">
-            <p class="reasons__list-title">
-                Наши услуги бесплатны
-            </p>
-        </li>
-        <li class="reasons__list-item">
-            <p class="reasons__list-title">
-                Цены ниже чем в страховой
-            </p>
-        </li>
-        <li class="reasons__list-item">
-            <p class="reasons__list-title">
-                Бесплатная доставка
-            </p>
-        </li>
-        <li class="reasons__list-item">
-            <p class="reasons__list-title">
-                Персональный менеджер каждому клиенту
-            </p>
-        </li>
-        <li class="reasons__list-item">
-            <p class="reasons__list-title">
-                Юридическая поддержка с защитой Ваших интересов
-            </p>
-        </li>
+    <ul class="plus_minus__list" style="padding-left: 65px">
+        <li>Наши услуги бесплатны</li>
+        <li>Цены ниже чем в страховой</li>
+        <li>Бесплатная доставка</li>
+        <li>Персональный менеджер каждому клиенту</li>
+        <li>Юридическая поддержка с защитой Ваших интересов</li>
     </ul>
-{{--    <div class="cooperation__btn-wrap">--}}
-{{--        <a href="/about" class="cooperation__link{{ isset($edit_mode) ? ' blockcrud-editable' : '' }}"--}}
-{{--        @if (isset($edit_mode))--}}
-{{--            contenteditable="true" id="main_reasons_2"--}}
-{{--        @endif--}}
-{{--        >{!! $main_reasons_2 ?? 'Детально о компании' !!}</a>--}}
-{{--    </div>--}}
-    </section>
+    {{--    <div class="cooperation__btn-wrap">--}}
+    {{--        <a href="/about" class="cooperation__link{{ isset($edit_mode) ? ' blockcrud-editable' : '' }}"--}}
+    {{--        @if (isset($edit_mode))--}}
+    {{--            contenteditable="true" id="main_reasons_2"--}}
+    {{--        @endif--}}
+    {{--        >{!! $main_reasons_2 ?? 'Детально о компании' !!}</a>--}}
+    {{--    </div>--}}
+</section>
