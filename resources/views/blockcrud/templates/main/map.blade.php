6e68b5d36d5edc298e15c46bc884e39beedc589e
levinpwnz <vkpasser@yandex.kz>
2021-01-21 21:39:32 +0800

    remove. text from map

--- resources/views/blockcrud/templates/main/map.blade.php
+++ resources/views/blockcrud/templates/main/map.blade.php
@@ -31,99 +31,28 @@
         <div class="contacts__wrapper glide__slides">
             <div class="contacts__map js-map glide__slide">
                 <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A0b75fc5d80c44e1955ed9540feb13208dee817641b958fc862006325c2cc8644&amp;width=100%25&amp;height=445&amp;lang=ru_RU&amp;scroll=false;baloon=true;"></script>
-                <div class="contacts__block js-contacts-block">
-                    <img src="/icon/cross.svg" alt="" class="contacts__close js-contacts-close">
-                    <div class="contacts__item">
-                        <span class="contacts__subtitle">Адрес: </span>
-                        <span class="">Москва, 3-й Монетчиковский переулок, дом 11, стр 1</span>
-                    </div>
-                    <div class="contacts__item">
-                        <span class="contacts__subtitle">Телефон:</span>+7 (495) 660-00-98, +7 (495) 62-62-555, +7&nbsp;(495) 626-26-26
-                        (отдел страхования доб. 714)
-                    </div>
-                    <div class="contacts__item">
-                        <span class="contacts__subtitle">График работы:</span>
-                        Пн-Пт: 9:00-19:00
-                    </div>
-                </div>
             </div>
             <div class="contacts__map js-map glide__slide">
                 <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A239c24f62b0f7f593773890c9cfc1af5d5556169f03455213e1b5d9c20596953&amp;width=100%25&amp;height=445&amp;lang=ru_RU&amp;scroll=false"></script>
-                <div class="contacts__block js-contacts-block">
-                    <img src="/icon/cross.svg" alt="" class="contacts__close js-contacts-close">
-                    <div class="contacts__item">
-                        <span class="contacts__subtitle">Адрес: </span>Санкт-Петербург, Боровая улица дом 32 , БЦ "Стелс", 7 этаж. 728 кабинет
-                    </div>
-                    <div class="contacts__item">
-                        <span class="contacts__subtitle">Телефон: </span>+7 812 449-43-25
-                    </div>
-                    <div class="contacts__item">
-                        <span class="contacts__subtitle">График работы: </span>
-                        Пн-Пт: 10:00-18:30
-                    </div>
-                </div>
             </div>
             <div class="contacts__map js-map glide__slide">
                 <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A78bfdc45b898e27e3d293878990b3bc6e88e86c6c7c85605f758869a5f7253d4&amp;width=100%25&amp;height=445&amp;lang=ru_RU&amp;scroll=false"></script>
-                <div class="contacts__block js-contacts-block">
-                    <img src="/icon/cross.svg" alt="" class="contacts__close js-contacts-close">
-                    <div class="contacts__item">
-                        <span class="contacts__subtitle">Адрес: </span>Нижний Новгород, ул. Костина, д.3
-                    </div>
-                    <div class="contacts__item">
-                        <span class="contacts__subtitle">Телефон: </span>+7 (831) 280-96-97
-                    </div>
-                    <div class="contacts__item">
-                        <span class="contacts__subtitle">График работы: </span>
-                        Пн-Вс: 9:00-19:00
-                    </div>
-                </div>
             </div>
             <div class="contacts__map js-map glide__slide">
                 <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ac9d10d3fe4225e5b31f6afd5a09a46aec8c68d8af738b81aa0516bee60923a9e&amp;width=100%25&amp;height=445&amp;lang=ru_RU&amp;scroll=false"></script>
-                <div class="contacts__block js-contacts-block">
-                    <img src="/icon/cross.svg" alt="" class="contacts__close js-contacts-close">
-                    <div class="contacts__item">
-                        <span class="contacts__subtitle">Адрес: </span>Рязань, ул. Маяковского д. 1а корп. Е БЦ "Капитал-Атрон"
-                    </div>
-                    <div class="contacts__item">
-                        <span class="contacts__subtitle">Телефон:</span>+7 (4912) 392-016
-                    </div>
-                    <div class="contacts__item">
-                        <span class="contacts__subtitle">График работы:</span>
-                        Пн-Пт: 9:00-19:00
-                    </div>
-                </div>
             </div>
             <div class="contacts__map js-map glide__slide">
                 <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A18f8a45551d3e32e77abaed520c711a832d388a7ca8e76e1e836b17c82bbfa27&amp;width=100%25&amp;height=445&amp;lang=ru_RU&amp;scroll=false"></script>
-                <div class="contacts__block js-contacts-block">
-                    <img src="/icon/cross.svg" alt="" class="contacts__close js-contacts-close">
-                    <div class="contacts__item">
-                        <span class="contacts__subtitle">Адрес: Воронеж, ул. Текстильщиков 5, стр.1</span>
-                    </div>
-                    <div class="contacts__item">
-                        <span class="contacts__subtitle">Телефон: +7 (473) 206-72-97</span> 
-                    </div>
-                    <div class="contacts__item">
-                        <span class="contacts__subtitle">График работы:</span>
-                        Пн-Пт: 9:00-19:00
-                    </div>
-                </div>
             </div>
             <div class="contacts__map js-map glide__slide">
                 <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Acbf05086f2ae4b63a1fe729e2e4736cb73822b0ad270212db5b481c2a29cb9e2&amp;width=100%25&amp;height=445&amp;lang=ru_RU&amp;scroll=false"></script>
+            </div>
+            <div class="contacts__map js-map glide__slide">
+
                 <div class="contacts__block js-contacts-block">
                     <img src="/icon/cross.svg" alt="" class="contacts__close js-contacts-close">
                     <div class="contacts__item">
-                        <span class="contacts__subtitle">Адрес: Казань, ул. Петербургская 74</span>
-                    </div>
-                    <div class="contacts__item">
-                        <span class="contacts__subtitle">Телефон: +7 (843) 567-19-11</span> 
-                    </div>
-                    <div class="contacts__item">
-                        <span class="contacts__subtitle">График работы:</span>
-                        Пн-Пт: 9:00-19:00
+                        <span class="contacts__subtitle">Телефон: +7 (463) 790-582</span>
                     </div>
                 </div>
             </div>
@@ -131,15 +60,7 @@
                 <div class="contacts__block js-contacts-block">
                     <img src="/icon/cross.svg" alt="" class="contacts__close js-contacts-close">
                     <div class="contacts__item">
-                        <span class="contacts__subtitle">Телефон: +7 (463) 790-582</span> 
-                    </div>
-                </div>
-            </div>
-            <div class="contacts__map js-map glide__slide">
-                <div class="contacts__block js-contacts-block">
-                    <img src="/icon/cross.svg" alt="" class="contacts__close js-contacts-close">
-                    <div class="contacts__item">
-                        <span class="contacts__subtitle">Телефон: +7 (612) 982-446</span> 
+                        <span class="contacts__subtitle">Телефон: +7 (612) 982-446</span>
                     </div>
                 </div>
             </div>
