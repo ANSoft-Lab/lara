5a9ef5ac81b712eee7b3046f8d17ae40ed4eec12
Dmitry^ <12>
2021-02-10 21:19:27 +0800

    landing

---
+++ resources/views/refin/refin.blade.php
@@ -0,0 +1,6 @@
+@extends('base')
+@section('content')
+    <div class="container-i" style="height: 100%; min-height: 100%">
+        <iframe id="ifr" class="iframe" src="http://refin.finist.ru/landing" width="100%" height="4200px" scrolling="no" style="min-height: 100%"></iframe>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,17 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+
+*/Route::get('/refin', function () {
+    return view('refin.refin');
+});
 
 Route::post('feedback_send', 'FeedbackController@store')->name('feedback_send');
 
 //Auth::routes();
 
 Route::group([
-    'prefix' => config('backpack.base.route_prefix', 'admin'), 
+    'prefix' => config('backpack.base.route_prefix', 'admin'),
     'middleware' => array_merge(
         (array) config('backpack.base.web_middleware', 'web'),
         (array) config('backpack.base.middleware_key', 'admin')
