6db03a0b4e26272b98d92fe47359d76c98e7edc5
Dmitry^ <12>
2021-02-16 00:46:01 +0800

    polisgarant

--- resources/views/blockcrud/templates/main/partners_slider.blade.php
+++ resources/views/blockcrud/templates/main/partners_slider.blade.php
@@ -3,40 +3,8 @@
         <li class="partners__list-item">
             <a href="№" class="partners__link">
                 <picture>
-                    <source srcset="/icon/partners/logo-zeta.png" media="(min-width: 768px)">
-                    <img src="/icon/partners/logo-zeta-m.png" alt="tnkf" class="partners__logo">
-                </picture>
-            </a>
-        </li>
-        <li class="partners__list-item">
-            <a href="№" class="partners__link">
-                <picture>
-                    <source srcset="/icon/partners/logo-soglasie.png" media="(min-width: 768px)">
-                    <img src="/icon/partners/logo-soglasie-m.png" alt="tnkf" class="partners__logo">
-                </picture>
-            </a>
-        </li>
-        <li class="partners__list-item">
-            <a href="{{ config('app.old_url') }}/strahovie_kompanii_vibor_kasko/tinkoff_strakhovanie/" class="partners__link">
-                <picture>
-                    <source srcset="/icon/partners/tnkf.png" media="(min-width: 768px)">
-                    <img src="/icon/partners/tnkf-m.png" alt="tnkf" class="partners__logo">
-                </picture>
-            </a>
-        </li>
-        <li class="partners__list-item">
-            <a href="{{ config('app.old_url') }}/strahovie_kompanii_vibor_kasko/reso_garantiya/" class="partners__link">
-                <picture>
-                    <source srcset="/icon/partners/reso.png" media="(min-width: 768px)">
-                    <img src="/icon/partners/reso-m.png" alt="reso" class="partners__logo">
-                </picture>
-            </a>
-        </li>
-        <li class="partners__list-item">
-            <a href="{{ config('app.old_url') }}/strahovie_kompanii_vibor_kasko/ingosstrakh/" class="partners__link">
-                <picture>
-                    <source srcset="/icon/partners/ingos.png" media="(min-width: 768px)">
-                    <img src="/icon/partners/ingos-m.png" alt="ingosstrakh" class="partners__logo">
+                    <source srcset="/icon/partners/polis-garant.png" media="(min-width: 768px)">
+                    <img src="/icon/partners/polis-garant-m.png" alt="tnkf" class="partners__logo">
                 </picture>
             </a>
         </li>
@@ -83,6 +51,14 @@
         <li class="partners__list-item">
             <a href="№" class="partners__link">
                 <picture>
+                    <source srcset="/icon/partners/polis-garant.png" media="(min-width: 768px)">
+                    <img src="/icon/partners/polis-garant-m.png" alt="tnkf" class="partners__logo">
+                </picture>
+            </a>
+        </li>
+        <li class="partners__list-item">
+            <a href="№" class="partners__link">
+                <picture>
                     <source srcset="/icon/partners/logo-zeta.png" media="(min-width: 768px)">
                     <img src="/icon/partners/logo-zeta-m.png" alt="tnkf" class="partners__logo">
                 </picture>
@@ -117,6 +93,62 @@
                 <picture>
                     <source srcset="/icon/partners/ingos.png" media="(min-width: 768px)">
                     <img src="/icon/partners/ingos-m.png" alt="ingosstrakh" class="partners__logo">
+                </picture>
+            </a>
+        </li>
+        <li class="partners__list-item">
+            <a href="№" class="partners__link">
+                <picture>
+                    <source srcset="/icon/partners/polis-garant.png" media="(min-width: 768px)">
+                    <img src="/icon/partners/polis-garant-m.png" alt="tnkf" class="partners__logo">
+                </picture>
+            </a>
+        </li>
+        <li class="partners__list-item">
+            <a href="№" class="partners__link">
+                <picture>
+                    <source srcset="/icon/partners/logo-zeta.png" media="(min-width: 768px)">
+                    <img src="/icon/partners/logo-zeta-m.png" alt="tnkf" class="partners__logo">
+                </picture>
+            </a>
+        </li>
+        <li class="partners__list-item">
+            <a href="№" class="partners__link">
+                <picture>
+                    <source srcset="/icon/partners/logo-soglasie.png" media="(min-width: 768px)">
+                    <img src="/icon/partners/logo-soglasie-m.png" alt="tnkf" class="partners__logo">
+                </picture>
+            </a>
+        </li>
+        <li class="partners__list-item">
+            <a href="{{ config('app.old_url') }}/strahovie_kompanii_vibor_kasko/tinkoff_strakhovanie/" class="partners__link">
+                <picture>
+                    <source srcset="/icon/partners/tnkf.png" media="(min-width: 768px)">
+                    <img src="/icon/partners/tnkf-m.png" alt="tnkf" class="partners__logo">
+                </picture>
+            </a>
+        </li>
+        <li class="partners__list-item">
+            <a href="{{ config('app.old_url') }}/strahovie_kompanii_vibor_kasko/reso_garantiya/" class="partners__link">
+                <picture>
+                    <source srcset="/icon/partners/reso.png" media="(min-width: 768px)">
+                    <img src="/icon/partners/reso-m.png" alt="reso" class="partners__logo">
+                </picture>
+            </a>
+        </li>
+        <li class="partners__list-item">
+            <a href="{{ config('app.old_url') }}/strahovie_kompanii_vibor_kasko/ingosstrakh/" class="partners__link">
+                <picture>
+                    <source srcset="/icon/partners/ingos.png" media="(min-width: 768px)">
+                    <img src="/icon/partners/ingos-m.png" alt="ingosstrakh" class="partners__logo">
+                </picture>
+            </a>
+        </li>
+        <li class="partners__list-item">
+            <a href="№" class="partners__link">
+                <picture>
+                    <source srcset="/icon/partners/polis-garant.png" media="(min-width: 768px)">
+                    <img src="/icon/partners/polis-garant-m.png" alt="tnkf" class="partners__logo">
                 </picture>
             </a>
         </li>
