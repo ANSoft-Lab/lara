5096f216693e116d91a86b37798842e0def51a1b
Dmitry^ <12>
2021-02-16 14:53:36 +0800

    fix. 65

--- resources/views/blockcrud/templates/main/all_sorts.blade.php
+++ resources/views/blockcrud/templates/main/all_sorts.blade.php
@@ -1,17 +1,17 @@
-<section class="all-sorts">
-    <h2 class="all-sorts__title{{ isset($edit_mode) ? ' blockcrud-editable' : '' }}"
-    @if (isset($edit_mode))
-        contenteditable="true" id="main_sorts_1"
-    @endif
-    >
-    {!! $main_sorts_1 ?? 'Все виды страхования' !!}
-    </h2>
-    <a href="{{ config('app.old_url') }}" class="all-sorts__link{{ isset($edit_mode) ? ' blockcrud-editable' : '' }}"
-    @if (isset($edit_mode))
-        contenteditable="true" id="main_sorts_2"
-    @endif
+{{--<section class="all-sorts">--}}
+{{--    <h2 class="all-sorts__title{{ isset($edit_mode) ? ' blockcrud-editable' : '' }}"--}}
+{{--    @if (isset($edit_mode))--}}
+{{--        contenteditable="true" id="main_sorts_1"--}}
+{{--    @endif--}}
+{{--    >--}}
+{{--    {!! $main_sorts_1 ?? 'Все виды страхования' !!}--}}
+{{--    </h2>--}}
+{{--    <a href="{{ config('app.old_url') }}" class="all-sorts__link{{ isset($edit_mode) ? ' blockcrud-editable' : '' }}"--}}
+{{--    @if (isset($edit_mode))--}}
+{{--        contenteditable="true" id="main_sorts_2"--}}
+{{--    @endif--}}
 
-    >
-    {!! $main_sorts_2 ?? 'Смотреть все' !!}
-    </a>
-</section>
+{{--    >--}}
+{{--    {!! $main_sorts_2 ?? 'Смотреть все' !!}--}}
+{{--    </a>--}}
+{{--</section>--}}
