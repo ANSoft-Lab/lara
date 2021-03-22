886c69ea70031676e715ac45cccc5226cd18f808
Nikita <dubchenko.nk@gmail.com>
2021-03-22 11:54:44 +0200

    update menu links

--- resources/views/blockcrud/templates/main/additional_ins.blade.php
+++ resources/views/blockcrud/templates/main/additional_ins.blade.php
@@ -1,18 +1,18 @@
 <section class="additional">
     <h2 class="additional__title">Дополнительные виды страхования</h2>
     <div class="additional__list">
-        <a href="{{ config('app.old_url') }}/strahovanie/vzr/calc/" class="additional__item">
+        <a href="/strahovanie/vzr/calc" class="additional__item">
             <div class="additional__content">
                 <p class="additional__subtitle">Страхование туристов</p>
             </div>
         </a>
 
-        <a class="additional__item" href="{{ config('app.old_url') }}/strahovanie/dms/">
+        <a class="additional__item" href="/strahovanie/dms">
             <div class="additional__content">
                 <p class="additional__subtitle">Медицинское страхование</p>
             </div>
         </a>
-        <a class="additional__item" href="{{ config('app.old_url') }}/strahovanie/osago/">
+        <a class="additional__item" href="/strahovanie/osago">
             <div class="additional__content">
                 <p class="additional__subtitle">Зелёная карта</p>
             </div>
