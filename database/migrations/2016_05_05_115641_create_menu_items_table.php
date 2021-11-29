85ba460dc937fedfb6c231f49f1f905d1bc15275
Alexarina <shuvalova.alexandra@gmail.com>
2021-11-29 14:59:52 +0300

    Fix migrations for further use

--- database/migrations/2016_05_05_115641_create_menu_items_table.php
+++ database/migrations/2016_05_05_115641_create_menu_items_table.php
@@ -11,12 +11,15 @@ class CreateMenuItemsTable extends Migration
             $table->increments('id');
             $table->string('name', 100);
             $table->string('type', 20)->nullable();
+            $table->string('menu_type')->default('top')->nullable();
             $table->string('link', 255)->nullable();
             $table->integer('page_id')->unsigned()->nullable();
             $table->integer('parent_id')->unsigned()->nullable();
             $table->integer('lft')->unsigned()->nullable();
             $table->integer('rgt')->unsigned()->nullable();
             $table->integer('depth')->unsigned()->nullable();
+            $table->tinyInteger('publish')->default(1);
+            $table->bigInteger('deleted_by')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
