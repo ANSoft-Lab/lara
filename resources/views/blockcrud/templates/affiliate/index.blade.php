38de52998587800baac0a53c4442e99ffc37bd0a
Alexarina <shuvalova.alexandra@gmail.com>
2021-12-23 09:32:46 +0300

    Metrika for affiliate

--- resources/views/blockcrud/templates/affiliate/index.blade.php
+++ resources/views/blockcrud/templates/affiliate/index.blade.php
@@ -777,3 +777,43 @@
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 <script src="/new/js/main.js"></script>
 
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+        (function (d, w, c) {
+            var n = d.getElementsByTagName("script")[0],
+                s = d.createElement("script"),
+                f = function () { n.parentNode.insertBefore(s, n); };
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+            if (w.opera == "[object Opera]") {
+                d.addEventListener("DOMContentLoaded", f);
+            } else { f(); }
+
+            (w[c] = w[c] || []).push(function() {
+                try {
+                    w.yaCounter13262362 = new Ya.Metrika({id:13262362, enableAll: true, webvisor:true});
+                } catch(e) {}
+            });
+        })(document, window, "yandex_metrika_callbacks");
+    </script>
+    <noscript><div><img src="//mc.yandex.ru/watch/13262362" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
+
+
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+fbq('init', '1766595446895929');
+fbq('track', "PageView");</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1766595446895929&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+
