c4ed8b6c7d37fdb862ebb8b590f8187d7ab3dad5
Alexarina <shuvalova.alexandra@gmail.com>
2021-12-29 23:23:28 +0300

    Cities in mobile menu
    №642

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -50,33 +50,71 @@
     </div>
 
     <nav class="menu js-mobile-menu mobile-hidden">
-    <div class="container">
-        <ul class="menu__list">
-            @foreach (App\Models\MenuItem::getTree() as $item)
-                <li class="menu__item">
-                    <a href="{{ $item->url() }}" class="menu__link{{ $item->children->count() > 0 ? ' menu__link_drop-down js-mobile-show-button' : '' }}" data-show="js_show_menu_{{ $item->id }}">
-                        {{ $item->name }}
-                    </a>
-                    @if ($item->children->count() > 0)
-                        <div class="menu__wrap mobile-hidden js_show_menu_{{ $item->id }}">
-                            <ul class="menu__sublist">
-                                @foreach ($item->children as $child)
-                                    <li class="menu__subitem">
-                                        <a href="{{ $child->url() }}" class="menu__subitem-link">
-                                            {{ $child->name }}
-                                        </a>
-                                    </li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
-                </li>
-            @endforeach
-        </ul>
+        <div class="container">
+            <ul class="menu__list">
+                @foreach (App\Models\MenuItem::getTree() as $item)
+                    <li class="menu__item">
+                        <a href="{{ $item->url() }}" class="menu__link{{ $item->children->count() > 0 ? ' menu__link_drop-down js-mobile-show-button' : '' }}" data-show="js_show_menu_{{ $item->id }}">
+                            {{ $item->name }}
+                        </a>
+                        @if ($item->children->count() > 0)
+                            <div class="menu__wrap mobile-hidden js_show_menu_{{ $item->id }}">
+                                <ul class="menu__sublist">
+                                    @foreach ($item->children as $child)
+                                        <li class="menu__subitem">
+                                            <a href="{{ $child->url() }}" class="menu__subitem-link">
+                                                {{ $child->name }}
+                                            </a>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                    </li>
+                @endforeach
+            </ul>
 
-        <a class="menu__text" href="#">Страховой случай</a>
-        <a class="menu__phone" href="tel:84956600098">+7 495 660-00-98</a>
-        <div class="menu__descr">Москва</div>
-    </div>
+            <a class="menu__text" href="#">Страховой случай</a>
+            <div class="js-city-show city_moscow">
+                <a class="menu__phone" href="tel:+74956600098">+7 495 660-00-98</a>
+                <div class="menu__descr">Москва</div>
+            </div>
+            <div class="js-city-show city_spb hidden">
+                <a class="menu__phone" href="tel:+78124494325">+7 812 449-43-25</a>
+                <div class="menu__descr">Санкт-Петербург</div>
+            </div>
+            <div class="js-city-show city_nn hidden">
+                <a class="menu__phone" href="tel:+78312809697">+7 831 280-96-97</a>
+                <div class="menu__descr">Нижний Новгород</div>
+            </div>
+            <div class="js-city-show city_ryazan hidden">
+                <a class="menu__phone" href="tel:+74912392016">+7 (4912) 392-016</a>
+                <div class="menu__descr">Рязань</div>
+            </div>
+            <div class="js-city-show city_voronezh hidden">
+                <a class="menu__phone" href="tel:+74732067297">+7 (473) 206-72-97</a>
+                <div class="menu__descr">Воронеж</div>
+            </div>
+            <div class="js-city-show city_kazan hidden">
+                <a class="menu__phone" href="tel:+78435671911">+7 (843) 567-19-11</a>
+                <div class="menu__descr">Казань</div>
+            </div>
+            <div class="js-city-show city_ekaterinburg hidden">
+                <a class="menu__phone" href="tel:+73433792092">+7 (343) 379-20-92</a>
+                <div class="menu__descr">Екатеринбург</div>
+            </div>
+            <div class="js-city-show city_krasnodar hidden">
+                <a class="menu__phone" href="tel:+78612982446">+7 (8612) 982-446</a>
+                <div class="menu__descr">Краснодар</div>
+            </div>
+            <div class="js-city-show city_samara hidden">
+                <a class="menu__phone" href="tel:+78463790582">+7 (8463) 790-582</a>
+                <div class="menu__descr">Самара</div>
+            </div>
+            <div class="js-city-show city_lipetsk hidden">
+                <a class="menu__phone" href="tel:+74742200745">+7 (4742) 200-745</a>
+                <div class="menu__descr">Липецк</div>
+            </div>
+        </div>
     </nav>
 </header>
